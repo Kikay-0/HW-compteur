e2de29997dc1cbb38b9e39409d72f57fbdfbdd00
Kikay-0 <108093975+Kikay-0@users.noreply.github.com>
2022-09-20 10:03:50 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
+import './style.css';
+import { useState } from 'react';
+import GlobalStyle from './style.js';
+
+const GlobalStyle2 = createGlobalStyle`
+body {
+    font-family: Open sans;
+    font-size: 25px;
+    background: #131415;
+    color: white
+}
+ `;
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  // pour l'incrémentation du bouton
+  const incr = () => {
+    // ajoute +1 du compteur
+    setCounter(counter + 1);
+  };
+
+  return (
+    <>
+      <>
+        <GlobalStyle2 />
+        <div>
+          <h1>Hello World!</h1>
+          <p> Texte de présentation </p>
+          <button onClick={incr}>Cliquez ICI</button>
+          <p>
+            {counter == 1
+              ? 'Vous avez cliqué sur le bouton pour la première fois'
+              : `
+            Vous avez cliqué sur le bouton ${counter} fois.`}
+          </p>
+        </div>
+      </>
+    </>
+  );
+};
+
+export default App;
